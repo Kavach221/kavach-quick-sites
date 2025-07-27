d94c520561c2936befb83cb30a60d855a0afafc3
Kavach221 <kavachsharma22@gmail.com>
2025-07-27 20:47:21 +0530

    Create documents.js

---
+++ src/pages/documents.js
@@ -0,0 +1,30 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="5vsU5E6ZpC1QqZdbJujP4m9Rkc228KVxF0Kh0XviJDQ" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Websites by Kavach - ₹9999 Me Website</title>
+        <meta name="description" content="Website banwaye sirf ₹9999 me - Kavach Sharma se." />
+        <meta name="keywords" content="website, ₹9999 website India, Kavach Sharma" />
+        <meta name="author" content="Kavach Sharma" />
+        <meta property="og:title" content="Websites by Kavach - ₹9999 Me Website" />
+        <meta property="og:description" content="Best website design just ₹9999" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kavach-quick-sites.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Websites by Kavach - ₹9999 Me Website" />
+        <meta name="twitter:description" content="Get your stunning business site in ₹9999!" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+    }
